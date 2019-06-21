68ed4e4f52e8e05e5cc005070de76a87a13108eb
Степан <martyanovst@gmail.com>
2019-06-21 10:05:40 +0500

    initial commit

---
+++ DocumentIndex.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DocsReport
+{
+	class DocumentOccurrences
+	{
+		public string DocumentKey { get; set; }
+		public List<int> OccurrencePositions { get; set; }
+	}
+	class DocumentIndex
+	{
+		private SuffixTree<int> tree;
+		private List<int> input;
+
+		public DocumentIndex(IReadOnlyList<KeyValuePair<string, byte[]>> documents)
+		{
+			var intCast = documents.Select(d => d.Value.Select(x => (int)x)).ToList();
+			var inputAlphabet = Enumerable.Range(0, 256 + intCast.Count);
+			input = intCast.SelectMany((d, i) => d.Concat(new[] { 256 + i })).ToList();
+			tree = new SuffixTree<int>(input, inputAlphabet);
+		}
+		public List<DocumentOccurrences> ReportOccurrences(byte[] pattern)
+		{
+			var results = new List<DocumentOccurrences>();
+			return results;
+		}
+	}
+}
